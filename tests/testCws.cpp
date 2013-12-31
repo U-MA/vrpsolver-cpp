ba3b8c3d9d04b4e52179f298a5410f201fd31622
U-MA <g.0224incident@gmail.com>
2013-12-31 16:25:23 +0900

    論文に出ている例をテスト問題として使用

--- tests/testCws.cpp
+++ tests/testCws.cpp
@@ -16,10 +16,12 @@ TEST_GROUP(Cws)
     {
         vrp = (vrp_problem *)malloc(sizeof(vrp_problem));
         vrp->dist.cost = (int *)calloc(100, sizeof(int));
+        vrp->demand    = (int *)calloc(100, sizeof(int));
     }
 
     void teardown()
     {
+        free(vrp->demand);
         free(vrp->dist.cost);
         free(vrp);
     }
@@ -27,6 +29,37 @@ TEST_GROUP(Cws)
     void Vrp_SetCost(int first, int second, int value)
     {
         vrp->dist.cost[INDEX(first, second)] = value;
+    }
+
+    /* Applying to Monte Carlo Techniques to the Capacitated Vehicle
+     * Routing Problem Table 2.1, 2.2より */
+    void Vrp_SetProblem(void)
+    {
+        vrp->vertnum = 6;
+        vrp->edgenum = vrp->vertnum * (vrp->vertnum-1) / 2;
+
+        Vrp_SetCost(0, 1, 28);
+        Vrp_SetCost(0, 2, 31);
+        Vrp_SetCost(0, 3, 20);
+        Vrp_SetCost(0, 4, 25);
+        Vrp_SetCost(0, 5, 34);
+        Vrp_SetCost(1, 2, 21);
+        Vrp_SetCost(1, 3, 29);
+        Vrp_SetCost(1, 4, 26);
+        Vrp_SetCost(1, 5, 20);
+        Vrp_SetCost(2, 3, 38);
+        Vrp_SetCost(2, 4, 20);
+        Vrp_SetCost(2, 5, 32);
+        Vrp_SetCost(3, 4, 30);
+        Vrp_SetCost(3, 5, 27);
+        Vrp_SetCost(4, 5, 25);
+
+        vrp->capacity  = 100;
+        vrp->demand[1] = 37;
+        vrp->demand[2] = 35;
+        vrp->demand[3] = 30;
+        vrp->demand[4] = 25;
+        vrp->demand[5] = 32;
     }
 };
 
@@ -40,18 +73,13 @@ TEST(Cws, InitSavings)
 
 TEST(Cws, setSavingsValue)
 {
-    vrp->edgenum = 3;
-    vrp->vertnum = 3;
-
-    Vrp_SetCost(0, 1, 20);
-    Vrp_SetCost(0, 2, 30);
-    Vrp_SetCost(1, 2, 15);
+    Vrp_SetProblem();
 
     Savings s;
-    s.set(vrp, 1, 2);
-    LONGS_EQUAL(35, s.getValue());
+    s.set(vrp, 1, 5);
+    LONGS_EQUAL(42, s.getValue());
     LONGS_EQUAL(1, s.getEdge().first);
-    LONGS_EQUAL(2, s.getEdge().second);
+    LONGS_EQUAL(5, s.getEdge().second);
 }
 
 TEST(Cws, savingsCompare)
@@ -109,16 +137,9 @@ TEST(Cws, SetSizeFromVrpData)
 
 TEST(Cws, getEdgeFromSavingsList)
 {
-    vrp->vertnum = 4;
-
-    Vrp_SetCost(0, 1, 20);
-    Vrp_SetCost(0, 2, 30);
-    Vrp_SetCost(1, 2, 15);
-    Vrp_SetCost(0, 3, 40);
-    Vrp_SetCost(1, 3, 10);
-    Vrp_SetCost(2, 3, 30);
+    Vrp_SetProblem();
 
     SavingsList sl(vrp);
     LONGS_EQUAL(1, sl.getEdge().first);
-    LONGS_EQUAL(3, sl.getEdge().second);
+    LONGS_EQUAL(5, sl.getEdge().second);
 }
