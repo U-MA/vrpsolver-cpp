b6bcb6ca8fa53d031e785b78e6461c996a2b1882
U-MA <g.0224incident@gmail.com>
2014-01-01 20:57:18 +0900

    整列した

--- include/Vehicle.h
+++ include/Vehicle.h
@@ -14,13 +14,6 @@ using namespace std;
 
 class Vehicle
 {
-private:
-    static const int MAXSIZE = 130;
-    int route[MAXSIZE];
-    int routeSize;
-    int quantity;
-    bool isVisit[MAXSIZE];
-
 public:
     Vehicle(void);
     Vehicle(int customerSize);
@@ -29,15 +22,18 @@ public:
 
     bool empty(void) const;
     bool isVisitOne(int customer) const;
-
     bool visit(const vrp_problem *vrp, int customer);
-
-    int getQuantity(void) const;
-    int getRoute(int idx) const; /* 早期に実装しすぎた */
-
-    int computeCost(const vrp_problem *vrp) const;
-
+    int  getQuantity(void) const;
+    int  getRoute(int idx) const; /* 早期に実装しすぎた */
+    int  computeCost(const vrp_problem *vrp) const;
     void print(void) const;
+
+private:
+    static const int MAXSIZE = 130;
+    bool isVisit[MAXSIZE];
+    int  route[MAXSIZE];
+    int  routeSize;
+    int  quantity;
 };
 
 #endif /* VRPSOLVER_CPP_VEHICLE_H */
