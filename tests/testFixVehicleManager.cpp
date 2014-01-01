09762b251a324d535c2616be65b3afda69490426
U-MA <g.0224incident@gmail.com>
2014-01-01 12:21:21 +0900

    PASS TEST getSize

--- tests/testFixVehicleManager.cpp
+++ tests/testFixVehicleManager.cpp
@@ -15,7 +15,6 @@ TEST(FixVehicleManager, empty)
 {
     VehicleManager vm;
     CHECK_TRUE(vm.empty());
-    LONGS_EQUAL(0, vm.getSize());
 }
 
 TEST(FixVehicleManager, addVehicle)
@@ -24,5 +23,20 @@ TEST(FixVehicleManager, addVehicle)
     Vehicle v;
     vm.add(v);
     CHECK_FALSE(vm.empty());
+}
+
+TEST(FixVehicleManager, getSize)
+{
+    VehicleManager vm;
+    
+    LONGS_EQUAL(0, vm.getSize());
+
+    Vehicle vehicle;
+    vm.add(vehicle);
+
     LONGS_EQUAL(1, vm.getSize());
+
+    vm.add(vehicle);
+
+    LONGS_EQUAL(2, vm.getSize());
 }
