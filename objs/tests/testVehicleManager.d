da0a2aa89f0e0aed3278005df66445de6484397e
U-MA <g.0224incident@gmail.com>
2014-01-03 18:40:55 +0900

    VehicleManagerとFixVehicleManagerのテストを同じファイルに移動

--- objs/tests/testVehicleManager.d
+++ objs/tests/testVehicleManager.d
@@ -14,9 +14,9 @@ objs/tests/testVehicleManager.o: tests/testVehicleManager.cpp \
  cpputest-3.5/include/CppUTest/TestPlugin.h \
  cpputest-3.5/include/CppUTest/MemoryLeakWarningPlugin.h \
  cpputest-3.5/include/CppUTest/MemoryLeakDetectorNewMacros.h \
- symphony/vrp_types.h symphony/sym_proto.h symphony/vrp_common_types.h \
- symphony/vrp_cg_params.h symphony/vrp_lp_params.h \
- include/VehicleManager.h include/Vehicle.h
+ symphony/vrp_macros.h symphony/sym_proto.h symphony/vrp_types.h \
+ symphony/vrp_common_types.h symphony/vrp_cg_params.h \
+ symphony/vrp_lp_params.h include/VehicleManager.h include/Vehicle.h
 
 cpputest-3.5/include/CppUTest/MemoryLeakDetectorNewMacros.h:
 
@@ -48,9 +48,11 @@ cpputest-3.5/include/CppUTest/MemoryLeakWarningPlugin.h:
 
 cpputest-3.5/include/CppUTest/MemoryLeakDetectorNewMacros.h:
 
-symphony/vrp_types.h:
+symphony/vrp_macros.h:
 
 symphony/sym_proto.h:
+
+symphony/vrp_types.h:
 
 symphony/vrp_common_types.h:
 
--- tests/testVehicleManager.cpp
+++ tests/testVehicleManager.cpp
@@ -2,6 +2,7 @@
 
 extern "C"
 {
+#include "vrp_macros.h"
 #include "vrp_types.h"
 }
 
@@ -11,22 +12,13 @@ TEST_GROUP(VehicleManager)
 {
     VehicleManager vm;
     vrp_problem *vrp;
+    Vehicle v;
 
     void setup()
     {
-        vrp = (vrp_problem *)malloc(sizeof(vrp_problem));
-        vrp->vertnum = 10;
-        vrp->dist.cost = (int *)calloc(10, sizeof(int));
-        vrp->demand = (int *)calloc(10, sizeof(int));
-
-        vrp->capacity = 2000;
-
-        vrp->demand[1] = 1900;
-        vrp->demand[2] = 1100;
-
-        vrp->dist.cost[0] = 30; /* 0-1 */
-        vrp->dist.cost[1] = 20; /* 0-2 */
-        vrp->dist.cost[2] = 15; /* 1-2 */
+        vrp            = (vrp_problem *)malloc(sizeof(vrp_problem));
+        vrp->dist.cost = (int *)calloc(100, sizeof(int));
+        vrp->demand    = (int *)calloc(100, sizeof(int));
     }
 
     void teardown()
@@ -35,6 +27,43 @@ TEST_GROUP(VehicleManager)
         free(vrp->dist.cost);
         free(vrp);
     }
+
+    void Vrp_SetCost(int first, int second, int value)
+    {
+        vrp->dist.cost[INDEX(first, second)] = value;
+    }
+
+    /* Applying to Monte Carlo Techniques to the Capacitated Vehicle
+     * Routing Problem Table 2.1, 2.2より */
+    void Vrp_SetProblem(void)
+    {
+        vrp->vertnum = 6;
+        vrp->edgenum = vrp->vertnum * (vrp->vertnum-1) / 2;
+
+        Vrp_SetCost(0, 1, 28);
+        Vrp_SetCost(0, 2, 31);
+        Vrp_SetCost(0, 3, 20);
+        Vrp_SetCost(0, 4, 25);
+        Vrp_SetCost(0, 5, 34);
+        Vrp_SetCost(1, 2, 21);
+        Vrp_SetCost(1, 3, 29);
+        Vrp_SetCost(1, 4, 26);
+        Vrp_SetCost(1, 5, 20);
+        Vrp_SetCost(2, 3, 38);
+        Vrp_SetCost(2, 4, 20);
+        Vrp_SetCost(2, 5, 32);
+        Vrp_SetCost(3, 4, 30);
+        Vrp_SetCost(3, 5, 27);
+        Vrp_SetCost(4, 5, 25);
+
+        vrp->capacity  = 100;
+        vrp->demand[1] = 37;
+        vrp->demand[2] = 35;
+        vrp->demand[3] = 30;
+        vrp->demand[4] = 25;
+        vrp->demand[5] = 32;
+    }
+
 };
 
 
@@ -50,34 +79,43 @@ TEST(VehicleManager, computeTotalCostAtFirst)
 
 TEST(VehicleManager, computeTotalCostAfterOneVehicleVisit)
 {
+    Vrp_SetProblem();
+
     Vehicle v;
     v.visit(vrp, 1);
     vm.add(v);
-    LONGS_EQUAL(60, vm.computeTotalCost(vrp));
+    LONGS_EQUAL(56, vm.computeTotalCost(vrp));
 }
 
 TEST(VehicleManager, computeTotalCostAfterSomeVehicleVisit)
 {
+    Vrp_SetProblem();
+
     Vehicle v1, v2;
     v1.visit(vrp, 1);
     v2.visit(vrp, 2);
     vm.add(v1);
     vm.add(v2);
-    LONGS_EQUAL(100, vm.computeTotalCost(vrp));
+    LONGS_EQUAL(118, vm.computeTotalCost(vrp));
 }
 
 TEST(VehicleManager, notVisitAllCustomer)
 {
+    Vrp_SetProblem();
+
     CHECK_FALSE(vm.isVisitAll(vrp));
 }
 
 TEST(VehicleManager, visitAllCustomer)
 {
-    vrp->vertnum = 3;
+    Vrp_SetProblem();
 
     Vehicle v1, v2;
     v1.visit(vrp, 1);
-    v2.visit(vrp, 2);
+    v1.visit(vrp, 2);
+    v2.visit(vrp, 3);
+    v2.visit(vrp, 4);
+    v2.visit(vrp, 5);
     vm.add(v1);
     vm.add(v2);
     CHECK_TRUE(vm.isVisitAll(vrp));
@@ -85,6 +123,8 @@ TEST(VehicleManager, visitAllCustomer)
 
 TEST(VehicleManager, isVisit)
 {
+    Vrp_SetProblem();
+
     Vehicle v;
     v.visit(vrp, 1);
     vm.add(v);
@@ -95,4 +135,35 @@ TEST(VehicleManager, isVisit)
 TEST(VehicleManager, size)
 {
     LONGS_EQUAL(0, vm.size());
+}
+
+TEST(VehicleManager, empty)
+{
+    CHECK_TRUE(vm.empty());
+}
+
+TEST(VehicleManager, addVehicle)
+{
+    vm.add(v);
+    CHECK_FALSE(vm.empty());
+}
+
+TEST(VehicleManager, getSize)
+{
+    LONGS_EQUAL(0, vm.size());
+    vm.add(v);
+    LONGS_EQUAL(1, vm.size());
+    vm.add(v);
+    LONGS_EQUAL(2, vm.size());
+}
+
+TEST(VehicleManager, getVehicle)
+{
+    Vrp_SetProblem();
+
+    v.visit(vrp, 1);
+    CHECK_TRUE(v.isVisit(1));
+    vm.add(v);
+    Vehicle v2 = vm.getVehicle();
+    CHECK_TRUE(v2.isVisit(1));
 }
