6d95bb4bf7fbb1dfde047fea0ac601c16e9dbcc2
U-MA <g.0224incident@gmail.com>
2014-01-17 22:05:19 +0900

    device_vrpのテストを作成

---
+++ cuda-sample/sample_device_vrp.cu
@@ -0,0 +1,11 @@
+#include "../include/device_vrp.h"
+
+/* device上のvrpデータを作成して解放する */
+int main(int argc, char **argv)
+{
+    vrp_problem *device_vrp = createVrpOnDevice();
+
+    deleteVrpOnDeice(device_vrp);
+
+    return 0;
+}
