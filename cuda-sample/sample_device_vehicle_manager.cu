c9cf6569c905faf1a735d2816e4b6fd4eb9aa419
U-MA <g.0224incident@gmail.com>
2014-01-19 12:05:37 +0900

    テストを拡張

--- cuda-sample/sample_device_vehicle_manager.cu
+++ cuda-sample/sample_device_vehicle_manager.cu
@@ -1,5 +1,15 @@
 #include "VehicleManager.h"
 
+__global__
+void check(int expect, VehicleManager *device_vm, bool *err)
+{
+    if (expect == device_vm->size())
+        err = false;
+    else
+        err = true;
+}
+
+
 /* DRAM上にVehicleManagerを確保するためのテスト */
 int main(int argc, char **argv)
 {
@@ -9,6 +19,16 @@ int main(int argc, char **argv)
     cudaMalloc((void **)&device_vehicle_manager, sizeof(VehicleManager));
     cudaMemcpy(device_vehicle_manager, &host_vehicle_manager,
                sizeof(VehicleManager), cudaMemcpyHostToDevice);
+
+    bool *err;
+    cudaMalloc((void **)&err, sizeof(bool));
+
+    check<<<1,1>>>(host_vehicle_manager.size(), device_vehicle_manager, err);
+
+    bool host_err;
+    cudaMemcpy(&host_err, err, sizeof(bool), cudaMemcpyDeviceToHost);
+
+    printf("err is %d\n", host_err);
 
     return 0;
 }
