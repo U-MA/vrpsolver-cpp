34887789c0c04c17aad753fe4f6913eeaf6679c3
U-MA <g.0224incident@gmail.com>
2014-01-29 23:24:10 +0900

    BaseVrpに存在している関数を削除

--- include/host_vrp.h
+++ include/host_vrp.h
@@ -17,12 +17,6 @@ public:
     ~HostVrp(void);
 
     void Create(const char *file_path);
-
-    int customer_size(void)  const { return vrp_->vertnum-1; }
-    int vehicle_size(void)   const { return vrp_->numroutes; }
-    int capacity(void)       const { return vrp_->capacity;  }
-    int cost(int v1, int v2) const { return vrp_->dist.cost[INDEX(v1, v2)]; }
-    int demand(int v) const        { return vrp_->demand[v]; }
 };
 
 #endif /* VRPSOLVER_CPP_HOST_VRP_H */
