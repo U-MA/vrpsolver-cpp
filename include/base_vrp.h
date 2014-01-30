b498d927108f963ba13ad603d915a75545d11002
U-MA <g.0224incident@gmail.com>
2014-01-30 14:36:06 +0900

    BaseVrpを抽象クラスに変更

--- include/base_vrp.h
+++ include/base_vrp.h
@@ -1,26 +1,16 @@
 #ifndef VRPSOLVER_CPP_BASE_VRP_H
 #define VRPSOLVER_CPP_BASE_VRP_H
 
-extern "C"
-{
-#include "vrp_macros.h"
-#include "vrp_types.h"
-}
-
 class BaseVrp
 {
 public:
-    BaseVrp(void) : vrp_(NULL) {};
     virtual ~BaseVrp(void) {};
 
-    int customer_size(void) const  { return vrp_->vertnum-1; }
-    int vehicle_size(void) const   { return vrp_->numroutes; }
-    int capacity(void) const       { return vrp_->capacity; }
-    int cost(int v0, int v1) const { return vrp_->dist.cost[INDEX(v0, v1)]; }
-    int demand(int v) const        { return vrp_->demand[v]; }
-
-protected:
-    vrp_problem *vrp_;
+    int customer_size(void)  const = 0;
+    int vehicle_size(void)   const = 0;
+    int capacity(void)       const = 0;
+    int cost(int v0, int v1) const = 0;
+    int demand(int v)        const = 0;
 };
 
 #endif /* VRPSOLVER_CPP_BASE_VRP_H */
