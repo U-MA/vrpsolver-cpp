d22ae4979e851c25d52334bba5d8e76e2c613ca5
U-MA <g.0224incident@gmail.com>
2014-02-04 17:18:28 +0900

    CUDA用のシミュレーションを書いた。まだ動かない

--- src/device_simulator.cu
+++ src/device_simulator.cu
@@ -5,6 +5,7 @@
 __global__
 void simulation_kernel(DeviceVrp *, VehicleManager *, int *);
 
+
 DeviceSimulator::Run(const HostVrp& host_vrp, const VehicleManager& vm, int count)
 {
     DeviceVrp device_vrp(host_vrp);
@@ -32,4 +33,66 @@ DeviceSimulator::Run(const HostVrp& host_vrp, const VehicleManager& vm, int coun
 __global__ void simulation_kernel(DeviceVrp *device_vrp, VehicleManager *device_vms,
                                   int *device_costs)
 {
+    extern __shared__ int candidates[];
+    __shared__ int  candidate_size;
+    __shared__ bool isFail;
+
+    philox4x32_key_t key = {{ blockIdx.x, 0xdeadbeaf }};
+    philox4x32_ctr_t ctr = {{ 0, 0xf00dcafe, 0xdeadbead, 0xbeaff00d }};
+
+    if (threadIdx.x == 0)
+    {
+        isFail = false;
+        candidate_size = 0;
+    }
+    __syncthreads();
+
+    const int vertnum = device_vrp->customer_size()+1;
+    while(isFail || !device_vms[blockIdx.x].isFinish(device_vrp))
+    {
+        /* 候補者の選定 */
+        if ((0 < threadIdx.x) && (threadIdx.x < vertnum) &&
+            !device_vms[blockIdx.x].isVisit(threadIdx.x) &&
+            device_canVisitCustomer(device_vms[blockIdx.x], threadIdx.x))
+        {
+            int old = atomicAdd(&candidates_size, 1);
+            candidates[old] = threadIdx.x;
+        }
+        __syncthreads();
+
+        /* 候補者を一人選ぶ */
+        if (threadIdx.x == 0)
+        {
+            if (candidate_size == 0)
+            {
+                if (!device_vms[blockIdx.x].nextVehicleRemain(device_vrp))
+                {
+                    isFail = !isFail;
+                }
+            }
+            else
+            {
+                union
+                {
+                    philox4x32_ctr_t c;
+                    int4 i;
+                } u;
+                ctr.v[0]++;
+
+                u.c = philox4x32(ctr, key);
+                int selected = candidate[u.c[0] % candidate_size];
+                device_vms[blockIdx.x].move(device_vrp, selected);
+            }
+            candidate_size = 0;
+        }
+        __syncthreads();
+    }
+
+    if (threadIdx.x == 0)
+    {
+        if (device_vms[blockIdx.x].isVisitAll(device_vrp))
+            device_costs[blockIdx.x] = device_vms[blockIdx.x].computeTotalCost(device_vrp);
+        else
+            device_costs[blockIdx.x] = 1000000;
+    }
 }
