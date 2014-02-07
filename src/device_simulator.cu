9bdb88006b92304441e8528e3a63583d6ab13ad1
U-MA <g.0224incident@gmail.com>
2014-02-07 20:44:59 +0900

    DeviceSimulator::Run()を実装

--- src/device_simulator.cu
+++ src/device_simulator.cu
@@ -15,25 +15,23 @@ DeviceSimulator::Run(const HostVrp& host_vrp, const VehicleManager& vm, int coun
 {
     DeviceVrp device_vrp(host_vrp);
 
-    thrust::device_vector<VehicleManager> device_vms(count);
-
-    /* vmの内容をdevice_vmsにコピー */
-    thrust::generate(device_vms.begin(), device_vms.end(), vm);
+    VehicleManager *device_vms = NULL;
+    cudaMalloc((void **)&device_vms, count * sizeof(VehicleManager));
+    for (int i=0; i < count; i++)
+        cudaMemcpy(&device_vms[i], &vm, sizeof(VehicleManager));
 
     thrust::device_vector<int> device_costs(count);
 
-    VehicleManager *device_vms_ptr = thrust::raw_pointer_cast(device_vms);
-    int *device_costs_ptr          = thrust::raw_pointer_cast(device_costs);
+    int *device_costs_ptr = thrust::raw_pointer_cast(device_costs);
 
     const int vertnum = host_vrp->customer_size()+1;
     simulation_kernel<<<count, vertnum, vertnum*sizeof(int)>>>(
-            device_vrp, device_vms_ptr, device_costs_ptr);
+            device_vrp, device_vms, device_costs_ptr);
 
     return thrust::reduce(device_costs_ptr.begin(),
                           device_costs_ptr.end(),
                           thrust::min_element());
 }
-
 
 __global__ void simulation_kernel(DeviceVrp *device_vrp, VehicleManager *device_vms,
                                   int *device_costs)
