deb53e9b238c7e7915132669c215e9e7d101502a
U-MA <g.0224incident@gmail.com>
2014-02-17 23:50:28 +0900

    将来使うテストを作成

--- tests/solution_test.cpp
+++ tests/solution_test.cpp
@@ -2,10 +2,40 @@
 
 #include "host_vrp.h"
 #include "solution.h"
+#include "vehicle.h"
 
 TEST_GROUP(Solution)
 {
 };
+
+/* まだ実装していない関数があるためコメントアウト
+TEST(Solution, IsFinish)
+{
+    HostVrp vrp("./Vrp-All/E/E-n13-k4.vrp");
+    Solution solution(vrp);
+
+    solution.current_vehicle()->visit(vrp, 1);
+
+    solution.ChangeVehicle();
+    solution.current_vehicle()->visit(vrp, 8);
+    solution.current_vehicle()->visit(vrp, 5);
+    solution.current_vehicle()->visit(vrp, 3);
+
+    solution.ChangeVehicle();
+    solution.current_vehicle()->visit(vrp, 9);
+    solution.current_vehicle()->visit(vrp, 12);
+    solution.current_vehicle()->visit(vrp, 10);
+    solution.current_vehicle()->visit(vrp, 6);
+
+    solution.ChangeVehicle();
+    solution.current_vehicle()->visit(vrp, 11);
+    solution.current_vehicle()->visit(vrp, 4);
+    solution.current_vehicle()->visit(vrp, 7);
+    solution.current_vehicle()->visit(vrp, 2);
+
+    CHECK_TRUE(solution.IsFinish());
+}
+*/
 
 TEST(Solution, IsNotFinishWhenCreate)
 {
