c6002346c30b771ab655c1568ca775ae705a502c
U-MA <g.0224incident@gmail.com>
2014-02-18 21:53:32 +0900

    テストコードの重複を削除

--- tests/solution_test.cpp
+++ tests/solution_test.cpp
@@ -6,42 +6,46 @@
 
 TEST_GROUP(Solution)
 {
-    HostVrp vrp;
+    HostVrp  vrp;
+    Solution *solution;
 
     void setup()
     {
         vrp.Create("./Vrp-All/E/E-n13-k4.vrp");
+        solution = new Solution(vrp);
+    }
+
+    void teardown()
+    {
+        delete solution;
     }
 };
 
 TEST(Solution, IsFinish)
 {
-    Solution solution(vrp);
+    solution->current_vehicle()->visit(vrp, 1);
 
-    solution.current_vehicle()->visit(vrp, 1);
+    solution->ChangeVehicle();
+    solution->current_vehicle()->visit(vrp, 8);
+    solution->current_vehicle()->visit(vrp, 5);
+    solution->current_vehicle()->visit(vrp, 3);
 
-    solution.ChangeVehicle();
-    solution.current_vehicle()->visit(vrp, 8);
-    solution.current_vehicle()->visit(vrp, 5);
-    solution.current_vehicle()->visit(vrp, 3);
+    solution->ChangeVehicle();
+    solution->current_vehicle()->visit(vrp, 9);
+    solution->current_vehicle()->visit(vrp, 12);
+    solution->current_vehicle()->visit(vrp, 10);
+    solution->current_vehicle()->visit(vrp, 6);
 
-    solution.ChangeVehicle();
-    solution.current_vehicle()->visit(vrp, 9);
-    solution.current_vehicle()->visit(vrp, 12);
-    solution.current_vehicle()->visit(vrp, 10);
-    solution.current_vehicle()->visit(vrp, 6);
+    solution->ChangeVehicle();
+    solution->current_vehicle()->visit(vrp, 11);
+    solution->current_vehicle()->visit(vrp, 4);
+    solution->current_vehicle()->visit(vrp, 7);
+    solution->current_vehicle()->visit(vrp, 2);
 
-    solution.ChangeVehicle();
-    solution.current_vehicle()->visit(vrp, 11);
-    solution.current_vehicle()->visit(vrp, 4);
-    solution.current_vehicle()->visit(vrp, 7);
-    solution.current_vehicle()->visit(vrp, 2);
-
-    CHECK_TRUE(solution.IsFinish());
+    CHECK_TRUE(solution->IsFinish());
 }
 
 TEST(Solution, IsNotFinishWhenCreate)
 {
-    Solution solution(vrp);
-    CHECK_FALSE(solution.IsFinish());
+    CHECK_FALSE(solution->IsFinish());
 }
