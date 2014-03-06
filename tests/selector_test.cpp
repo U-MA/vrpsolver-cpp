2b655b84832419eb14651a7f596d495774b2675a
U-MA <g.0224incident@gmail.com>
2014-03-06 19:51:52 +0900

    ノード数３の一直線の木を探索可能になった

--- tests/selector_test.cpp
+++ tests/selector_test.cpp
@@ -41,3 +41,21 @@ TEST(Selector, ucb_selector_3)
     POINTERS_EQUAL(&root, visited[0]);
     POINTERS_EQUAL(root.Child(1), visited[1]);
 }
+
+TEST(Selector, ucb_selector_4)
+{
+    MctNode root(0);
+    root.CreateChild(0);
+    MctNode *child = root.Child(0);
+    child->CreateChild(0);
+
+    root.Update(100);
+    root.Update(100);
+    root.Child(0)->Update(100);
+    child->Child(0)->Update(100);
+
+    POINTERS_EQUAL(child->Child(0), Selector::Ucb(root, visited));
+    POINTERS_EQUAL(&root, visited[0]);
+    POINTERS_EQUAL(child, visited[1]);
+    POINTERS_EQUAL(child->Child(0), visited[2]);
+}
