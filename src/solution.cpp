d8f77b04e29bbfe25f3917b07d2bf980a8a7f078
U-MA <g.0224incident@gmail.com>
2014-03-07 23:50:58 +0900

    全ての顧客を訪問していなければ実行可能解では無い

--- src/solution.cpp
+++ src/solution.cpp
@@ -24,7 +24,17 @@ void Solution::ChangeVehicle()
 
 bool Solution::IsFeasible() const
 {
-    return IsFinish();
+    for (int i=1; i <= customer_size_; i++)
+    {
+        for (int j=0; j <= current_vehicle_id_; j++)
+        {
+            if (vehicles_[j].is_visit(i))
+                break;
+            if (j == current_vehicle_id_)
+                return false;
+        }
+    }
+    return true;
 }
 
 bool Solution::IsFinish() const
